2f8421310a46102e2cc818426c15a896e1983915
msaf1980 <msaf1980@gmail.com>
2021-06-11 14:49:25 +0500

    tests: switch to ctest

--- tests/test_counter.c
+++ tests/test_counter.c
@@ -1,4 +1,4 @@
-#include <check.h>
+#include "ctest.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -9,47 +9,43 @@
 #include <math.h>
 #include "counter.h"
 
-START_TEST(test_counter_init)
+CTEST(counter, counter_init)
 {
     counter c;
     int res = init_counter(&c);
-    fail_unless(res == 0);
+    ASSERT_TRUE(res == 0);
 }
-END_TEST
 
-START_TEST(test_counter_init_add)
+CTEST(counter, counter_init_add)
 {
     counter c;
     int res = init_counter(&c);
-    fail_unless(res == 0);
+    ASSERT_TRUE(res == 0);
 
-    fail_unless(counter_add_sample(&c, 100, 1.0) == 0);
-    fail_unless(counter_sum(&c) == 100);
+    ASSERT_TRUE(counter_add_sample(&c, 100, 1.0) == 0);
+    ASSERT_TRUE(counter_sum(&c) == 100);
 }
-END_TEST
 
-START_TEST(test_counter_add_loop)
+CTEST(counter, counter_add_loop)
 {
     counter c;
     int res = init_counter(&c);
-    fail_unless(res == 0);
+    ASSERT_TRUE(res == 0);
 
     for (int i=1; i<=100; i++)
-        fail_unless(counter_add_sample(&c, i, 1.0) == 0);
+        ASSERT_TRUE(counter_add_sample(&c, i, 1.0) == 0);
 
-    fail_unless(counter_sum(&c) == 5050);
+    ASSERT_TRUE(counter_sum(&c) == 5050);
 }
-END_TEST
 
-START_TEST(test_counter_sample_rate)
+CTEST(counter, counter_sample_rate)
 {
     counter c;
     int res = init_counter(&c);
-    fail_unless(res == 0);
+    ASSERT_TRUE(res == 0);
 
     for (int i=1; i<=100; i++)
-        fail_unless(counter_add_sample(&c, i, 0.5) == 0);
+        ASSERT_TRUE(counter_add_sample(&c, i, 0.5) == 0);
 
-    fail_unless(counter_sum(&c) == 5050);
+    ASSERT_TRUE(counter_sum(&c) == 5050);
 }
-END_TEST
